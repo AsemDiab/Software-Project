392315a2eb0fdc77ac00aa691e8501b3a300b07d
sayedpalestine7 <sayed-zx7@hotmail.com>
2024-02-26 09:06:46 +0200

    Starting page js files

---
+++ JS-Files/StartingPage/login.js
@@ -0,0 +1,16 @@
+const { Given, When, Then } = require("@cucumber/cucumber");
+
+Given("the user is in the startng page", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+When("the user enters {string}", function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then("transfere him to the login page", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
