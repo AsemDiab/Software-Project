2619ec001c94b4dda308efaea40e2a41cba71904
ahmadD301 <alaaedeen84@gmail.com>
2024-03-16 01:25:45 +0200

    add myAccount controller and build its feature

---
+++ JS-Files/myAccountP.js
@@ -0,0 +1,65 @@
+const readlineSync = require("readline-sync");
+const DB = require("../JS-Files/ourDataBase");
+const Page = require("../JS-Files/Page.js");
+
+class myAccount extends Page{
+
+    creatAccount = false;
+    loginAccount = false;
+    returnFlage = false
+
+    init(){
+        this.creatAccount = false;
+        this.loginAccount = false;
+        returnFlage = false
+    }
+
+    userProfileInfo(){
+        console.log('display user account info');
+    }
+
+    creatBusinessAccount(){
+        this.creatAccount = true;
+    }
+    loginBusinessAccount(){
+        this.loginAccount = true;
+    }
+    returnBack(){
+        this.returnFlage = true;
+    }
+
+    printMenu(){
+        console.log(`
+        select the button by enter the number: 
+            0: user info
+            1: Create business account
+            2: login business account
+            3: return
+        `);
+    }
+    
+    run(theAction){
+        switch(theAction.trim()){
+            case "user info":
+                this.userProfileInfo();
+                console.log('in user info case');
+                break;
+            case "Create business account":
+                this.creatBusinessAccount();
+                console.log('in create business account case');
+                break;
+            case "login business account":
+                this.loginBusinessAccount();
+                console.log('in login business account case');
+                break;
+            case "return":
+                this.returnBack();
+                console.log('in return case');
+                break;
+            default:
+                console.log("invalid input");
+        }   
+    }
+}
+
+module.exports = myAccount;
--- features/myAccount.feature
+++ features/myAccount.feature
@@ -2,18 +2,23 @@ Feature: My Account
 
     Scenario: Show user profile info
         Given the user in my account page
-        When select "user info" option
+        When select "user info" options
         Then display all user information 
 
     Scenario: Create business account 
         Given the user in my account page
-        When select "Create business account" option
+        When select "Create business account" options
         Then send him to Create business account page
 
     Scenario: login to businss account
         Given the user in my account page
-        When select "login business account" option
+        When select "login business account" options
         Then send him to business account page
+    
+    Scenario: return
+        Given the user in my account page
+        When select "return" options
+        Then bake to user home page
 
     Scenario Outline: Invalid input
         Given the user in my account page
